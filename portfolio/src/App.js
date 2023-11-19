0307759f7fa34507008648d691afe535ce2a58dc
Tuturtut <arthursimonin88@gmail.com>
2023-11-19 20:15:38 +0100

    Oui

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import ProjetctCard from './component/ProjectCard';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="text-3xl font-bold underline">Simonin Arthur</h1>
-
       </header>
     </div>
   );
---
+++ portfolio/src/component/ProjectCard.js
@@ -0,0 +1,9 @@
+// Work component
+import React from 'react';
+function ProjectCard() {
+    return (
+        <div className="work">
+            <h1>Work</h1>
+        </div>
+    );
+}
