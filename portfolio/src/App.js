2d31c7d7c780ff90fea8500c76356fa8156a4a6d
Tuturtut <arthursimonin88@gmail.com>
2023-11-20 16:55:19 +0100

    Add arrows

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,17 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
-import ProjetctCard from './component/ProjectCard';
 function App() {
+  const arrowNumber = 15; // Remplace avec le nombre souhaité
+
+
   return (
     <div className="App">
-
-
       <body>
         <h1 className="Title">ARTHUR SIMONIN</h1>
         <h1 className="Project">Projects</h1>
+        <div className="arrows-right">
+          {getArrows(arrowNumber, "right")}
+        </div>
+        <div className="line">
+          <hr />
+        </div>
+        <div className="arrows-left">
+          {getArrows(arrowNumber, "left")}
+        </div>
+
+
+
       </body>
     </div>
   );
+}
+
+function getArrows(number, direction) {
+
+  const fleches = [];
+  for (let i = 0; i < number; i++) {
+    fleches.push(<i key={i} className={"arrow " + direction}></i>);
+  }
+  return fleches;
 }
 
 export default App;
--- portfolio/src/component/ProjectCard.js
+++
@@ -1,9 +0,0 @@
-// Work component
-import React from 'react';
-function ProjectCard() {
-    return (
-        <div className="work">
-            <h1>Work</h1>
-        </div>
-    );
-}
